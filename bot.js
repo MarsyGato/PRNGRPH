0ab6f60b9c5451e8f0397d392be26f62348fc06b
MarsyGato <37179923+MarsyGato@users.noreply.github.com>
2018-03-08 22:59:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,15 +11,15 @@ bot.on('message', message => {
 var pic Math.floor(Math.random() * 2);
     } 
     
-      if (pic === '2') {
+      if (pic === 2) {
       message.channel.send({
       file: 'https://cdn.discordapp.com/attachments/374203224498110468/421248886649257984/wallhaven-389981.jpg'});
 
-      if (pic === '1') {
+      if (pic === 1) {
       message.channel.send({
       file: 'https://cdn.discordapp.com/attachments/353588490866262016/421272481077395456/porn.gif'});
         
-      if (pic === '0') {
+      if (pic === 0) {
       message.channel.send({
       file: 'https://cdn.discordapp.com/attachments/374203224498110468/421295090237964297/wallhaven-598110.jpg'});
   
