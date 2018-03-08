df9a3e9920f6cc4975681cd27686284236bb2830
MarsyGato <37179923+MarsyGato@users.noreply.github.com>
2018-03-08 22:29:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,25 @@ bot.login(process.env.TOKEN)
 
 bot.on('message', message => {
   if (message.content === '!lu$t') {
-    message.channel.send({
-    file: 'https://cdn.discordapp.com/attachments/353588490866262016/421272481077395456/porn.gif'});
-  }
-})
+ 
+    
+    pic = getRndInteger(0,2);
+    function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+    } 
+    
+      if (pic === '2') {
+      message.channel.send({
+      file: 'https://cdn.discordapp.com/attachments/374203224498110468/421248886649257984/wallhaven-389981.jpg'});
 
+      if (pic === '1') {
+      message.channel.send({
+      file: 'https://cdn.discordapp.com/attachments/353588490866262016/421272481077395456/porn.gif'});
+        
+      if (pic === '0') {
+      message.channel.send({
+      file: 'https://cdn.discordapp.com/attachments/374203224498110468/421295090237964297/wallhaven-598110.jpg'});
+  
+}}
+  
+})
