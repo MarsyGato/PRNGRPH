8a2bb53ba7206f141b8196dd92237d641f0b516e
MarsyGato <37179923+MarsyGato@users.noreply.github.com>
2018-03-08 14:22:21 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,7 @@ bot.login(process.env.TOKEN)
 
 bot.on('message', message => {
   if (message.content === '!lu$t') {
-    message.reply('https://cdn.discordapp.com/attachments/374203224498110468/421097072905682955/wallhaven-389981.jpg')
     message.channel.send({
-    file: 'https://cdn.discordapp.com/attachments/374203224498110468/421255464542666764/wallhaven-598110.jpg'});
+    file: 'https://cdn.discordapp.com/attachments/353588490866262016/421272481077395456/porn.gif'});
   }
 })
