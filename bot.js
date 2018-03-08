41967e6c8a995f0733caea5c8cd9614137aeaa1d
MarsyGato <37179923+MarsyGato@users.noreply.github.com>
2018-03-08 13:16:36 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+
+
+bot.login('NDIxMDc2OTI0ODQ1MjYwODE2.DYIFqw.EpPHTbAhRQWFTNOM95p3irrzhxg')
+
+bot.on('message', message => {
+  if (message.content === '!lu$t') {
+    message.reply('https://cdn.discordapp.com/attachments/374203224498110468/421097072905682955/wallhaven-389981.jpg')
+    message.channel.send({
+    file: 'https://cdn.discordapp.com/attachments/374203224498110468/421255464542666764/wallhaven-598110.jpg'});
+  }
+})
